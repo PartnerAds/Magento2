b9644ce09bfb83a76771a2682f0685e8c3dbf3f6
Julian F. Christmas <jc@intelligodenmark.dk>
2024-03-15 20:23:44 +0100

    Ver 2.2.0

--- Model/Request.php
+++ Model/Request.php
@@ -5,14 +5,14 @@
 
 namespace Partner\Module\Model;
 
+use Laminas\Http\Client\Adapter\Curl;
+use Laminas\Http\Client as LaminasHttpClient;
+use Laminas\Http\Request as LaminasHttpRequest;
+use Laminas\Stdlib\Parameters as LaminasRequestParameters;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\Order;
 use Magento\Store\Model\StoreManagerInterface;
 use Partner\Module\Api\ConfigInterface;
-use Zend\Http\Request as ZendHttpRequest;
-use Zend\Stdlib\Parameters as ZendRequestParameters;
-use Zend\Http\Client as ZendHttpClient;
-use Zend\Http\Client\Adapter\Curl;
 use Partner\Module\Model\System\Config\Mode;
 
 class Request
@@ -34,7 +34,7 @@ class Request
      * @var StoreManagerInterface
      */
     private $storeManager;
-    
+
     /**
      * @var  \Magento\Framework\App\Config\ScopeConfigInterface
      */
@@ -42,6 +42,7 @@ class Request
 
     /**
      * Request constructor.
+     *
      * @param ConfigInterface $config
      * @param OrderRepositoryInterface $orderRepository
      * @param StoreManagerInterface $storeManager
@@ -60,8 +61,10 @@ class Request
 
     /**
      * @param Order $order
-     * @return bool
+     *
      * @throws SkipMessage
+     *
+     * @return bool
      */
     public function send(Order $order)
     {
@@ -70,65 +73,72 @@ class Request
         $programId = $this->config->getProgramId($store);
         $type = $this->config->getProgramType($store);
         $commitAtOrderStateSetting = $this->config->getProgramOrderState($store);
-    
+
         $requestUri = $this->config->getMode($store);
         $requestUri = Mode::TRACKING_URL;
         $this->debuggerOn("Endpoint = ".$requestUri, $this->config->getMode($store));
         // Set at least a default status when none/false/empty is given (set to STATE_PROCESSING)
-        if (empty($commitAtOrderStateSetting) || !$commitAtOrderStateSetting) {
+        if (empty($commitAtOrderStateSetting) || ! $commitAtOrderStateSetting) {
             $commitAtOrderState = Order::STATE_PROCESSING;
         } else {
             $commitAtOrderState = $commitAtOrderStateSetting;
         }
 
         if (empty($programId)) {
-             $this->debuggerOn(
-                 'Cannot Export: #' . $order->getIncrementId() .
-                 ' Because Store: ' . $store->getCode() . ' Is missing valid "program_id".',
-                 $this->config->getMode($store)
-             );
+            $this->debuggerOn(
+                'Cannot Export: #' . $order->getIncrementId() .
+                ' Because Store: ' . $store->getCode() . ' Is missing valid "program_id".',
+                $this->config->getMode($store)
+            );
+
             throw new SkipMessage('Cannot Export: #' . $order->getIncrementId() . ' Because Store: ' . $store->getCode() . ' Is missing valid "program_id".'); // @codingStandardsIgnoreLine
         }
 
         if (empty($type)) {
-             $this->debuggerOn(
-                 'Cannot Export: #' . $order->getIncrementId() .
-                 ' Because Store: ' . $store->getCode() . ' Is missing valid "program_type".',
-                 $this->config->getMode($store)
-             );
+            $this->debuggerOn(
+                'Cannot Export: #' . $order->getIncrementId() .
+                ' Because Store: ' . $store->getCode() . ' Is missing valid "program_type".',
+                $this->config->getMode($store)
+            );
+
             throw new SkipMessage('Cannot Export: #' . $order->getIncrementId() . ' Because Store: ' . $store->getCode() . ' Is missing valid "program_type".'); // @codingStandardsIgnoreLine
         }
 
         if ($order->getState() !== $commitAtOrderState) {
-             $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
-             ' Because Order State: "' . $order->getState() .
-             '" is not equal to Commit State: "' . $commitAtOrderState .
-             '".', $this->config->getMode($store));
+            $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
+            ' Because Order State: "' . $order->getState() .
+            '" is not equal to Commit State: "' . $commitAtOrderState .
+            '".', $this->config->getMode($store));
+
             throw new SkipMessage('Cannot Export: #' . $order->getIncrementId() . ' Because Order State: "' . $order->getState() . '" is not equal to Commit State: "' . $commitAtOrderState . '".'); // @codingStandardsIgnoreLine
         }
 
         if (0 != $order->getData(Attributes::ORDER_SENT)) {
-             $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
-             ' Because it is already sent.');
+            $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
+            ' Because it is already sent.');
+
             throw new SkipMessage('Cannot Export: #' . $order->getIncrementId() . ' Because it is already sent.'); // @codingStandardsIgnoreLine
         }
 
-        if (!$order->hasData(Attributes::PARTNER_ID) || empty($order->getData(Attributes::PARTNER_ID))) {
-             $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
-             ' It is missing a valid value in attribute "' . Attributes::PARTNER_ID .
-             '".', $this->config->getMode($store));
+        if (! $order->hasData(Attributes::PARTNER_ID) || empty($order->getData(Attributes::PARTNER_ID))) {
+            $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
+            ' It is missing a valid value in attribute "' . Attributes::PARTNER_ID .
+            '".', $this->config->getMode($store));
+
             throw new SkipMessage('Cannot Export: #' . $order->getIncrementId() . ' It is missing a valid value in attribute "' . Attributes::PARTNER_ID . '".'); // @codingStandardsIgnoreLine
         }
 
-        if (!filter_var($requestUri, FILTER_VALIDATE_URL)) {
-             $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
-             ' Because Store: ' . $store->getCode() . ' Is missing valid "mode".', $this->config->getMode($store));
+        if (! filter_var($requestUri, FILTER_VALIDATE_URL)) {
+            $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
+            ' Because Store: ' . $store->getCode() . ' Is missing valid "mode".', $this->config->getMode($store));
+
             throw new SkipMessage('Cannot Export: #' . $order->getIncrementId() . ' Because Store: ' . $store->getCode() . ' Is missing valid "mode".'); // @codingStandardsIgnoreLine
         }
 
         if (in_array($order->getId(), $this->exporting)) {
-             $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
-             'Because it is already exported.', $this->config->getMode($store));
+            $this->debuggerOn('Cannot Export: #' . $order->getIncrementId() .
+            'Because it is already exported.', $this->config->getMode($store));
+
             throw new SkipMessage('Cannot Export: #' . $order->getIncrementId() . 'Because it is already exported.'); // @codingStandardsIgnoreLine
         }
         $this->exporting[] = $order->getId();
@@ -136,67 +146,67 @@ class Request
         $partnerId = $order->getData(Attributes::PARTNER_ID);
         $pacid = $order->getData(Attributes::PACID);
 
-        $request = new ZendHttpRequest();
-        $request->setMethod(ZendHttpRequest::METHOD_GET);
+        $request = new LaminasHttpRequest();
+        $request->setMethod(LaminasHttpRequest::METHOD_GET);
         $request->setUri($requestUri);
-        
-         $exportData = [
-            'programid' => $programId,
-            'type' => $type,
-            'partnerid' => $partnerId,
-            'pacid' => $pacid ? $pacid : '',
-            'ordreid' => $order->getRealOrderId(),
-            'varenummer' => 'x',
-            'antal' => 1,
-            'omprsalg' => $order->getGrandTotal() - $order->getShippingInclTax(),
-         ];
-        
-         $this->debuggerOn("Request=".json_encode($exportData), $this->config->getMode($store));
 
-         $parameters = new ZendRequestParameters($exportData);
-         $request->setQuery($parameters);
+        $exportData = [
+           'programid' => $programId,
+           'type' => $type,
+           'partnerid' => $partnerId,
+           'pacid' => $pacid ? $pacid : '',
+           'ordreid' => $order->getRealOrderId(),
+           'varenummer' => 'x',
+           'antal' => 1,
+           'omprsalg' => $order->getGrandTotal() - $order->getShippingInclTax(),
+        ];
 
-         $client = new ZendHttpClient();
-         $options = [
-            'adapter' => Curl::class,
-            'curloptions' => [CURLOPT_FOLLOWLOCATION => true],
-            'maxredirects' => 0,
-            'timeout' => 10
-         ];
-         $client->setOptions($options);
-        
-         $response = $client->send($request);
-         $this->debuggerOn("Response=".$response, $this->config->getMode($store));
+        $this->debuggerOn("Request=".json_encode($exportData), $this->config->getMode($store));
 
-         $responseNumber = floor($response->getStatusCode() / 100);
+        $parameters = new LaminasRequestParameters($exportData);
+        $request->setQuery($parameters);
 
-         if (2 == $responseNumber) {//check if response code is a 2XX code
-             $order->setData(Attributes::ORDER_SENT, 1);
-             $this->orderRepository->save($order);
+        $client = new LaminasHttpClient();
+        $options = [
+           'adapter' => Curl::class,
+           'curloptions' => [CURLOPT_FOLLOWLOCATION => true],
+           'maxredirects' => 0,
+           'timeout' => 10,
+        ];
+        $client->setOptions($options);
 
-             $key = array_search($order->getId(), $this->exporting);
+        $response = $client->send($request);
+        $this->debuggerOn("Response=".$response, $this->config->getMode($store));
 
-             if (false !== $key) {
-                 unset($this->exporting[$key]);
-             }
+        $responseNumber = floor($response->getStatusCode() / 100);
 
-             return true;
-         } else {
-             $failedExportException = new SkipMessage('Failed to export: #' . $order->getIncrementId() . '. Response: "' . $response->renderStatusLine() . '".'); // @codingStandardsIgnoreLine
+        if (2 == $responseNumber) {//check if response code is a 2XX code
+            $order->setData(Attributes::ORDER_SENT, 1);
+            $this->orderRepository->save($order);
 
-             $failedExportException->setExportData($exportData);
+            $key = array_search($order->getId(), $this->exporting);
 
-             throw $failedExportException;
-         }
+            if (false !== $key) {
+                unset($this->exporting[$key]);
+            }
+
+            return true;
+        } else {
+            $failedExportException = new SkipMessage('Failed to export: #' . $order->getIncrementId() . '. Response: "' . $response->renderStatusLine() . '".'); // @codingStandardsIgnoreLine
+
+            $failedExportException->setExportData($exportData);
+
+            throw $failedExportException;
+        }
     }
-    
+
     public function debuggerOn($str, $requestUri = null)
     {
-        
+
         if ($requestUri == Mode::DEBUG_URL) {
-            
-            $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/Partnerads.log');
-            $logger = new \Zend_Log();
+
+            $writer = new \Laminas\Log\Writer\Stream(BP . '/var/log/Partnerads.log');
+            $logger = new \Laminas\Log\Logger();
             $logger->addWriter($writer);
             $logger->info($str);
         }
--- README.md
+++ README.md
@@ -1,53 +1,60 @@
-
-# PartnerAds - Magento 2.4+ Affiliate tracking
+# PartnerAds - Magento 2.3+ Affiliate tracking
 
 This extension adds the following functionality to Magento:
 
--   Track visitors from Partner Ads publishers via cookie
--   Send order information to Partner Ads affiliate network depending on order status
--   Possibility for communication Magento backend via GraphQL endpoints
+- Track visitors from Partner Ads publishers via cookie
+- Send order information to Partner Ads affiliate network depending on order status
+- Possibility for communication Magento backend via GraphQL endpoints
 
-The plugin is tested and confirmed working on
+## Compatibility
 
--   Magento 2.4
+The Partner Ads affiliate tracking extension tested with:
+
+- Magento 2.3
+- Magento 2.4
+
+## For Magento versions below 2.3:
+
+For those running Magento versions earlier than 2.3, we recommend utilizing version 2.1 of this extension.
+This version maintains the essential features tailored to be compatible with older Magento instances, ensuring you can still leverage the benefits of the Partner Ads affiliate network without the need for an immediate platform upgrade.
 
 ## Usage
 
 ### Installation via Composer (Recommended)
 
- - Install the module composer by running `composer require partnerads/magento2`
- - Enable the module by running `php bin/magento module:enable Partner_Module`
- - Apply database updates by running `php bin/magento setup:upgrade`
- - Compile Magento code base by running `php bin/magento setup:di:compile`
- - Deploy static content by running `php bin/magento setup:static-content:deploy`
- - Clean the cache by running `php bin/magento cache:clean`
- - Setup Partner Program ID via Magento Admin panel
- - You are ready to go
+- Install the module composer by running `composer require partnerads/magento2`
+- Enable the module by running `php bin/magento module:enable Partner_Module`
+- Apply database updates by running `php bin/magento setup:upgrade`
+- Compile Magento code base by running `php bin/magento setup:di:compile`
+- Deploy static content by running `php bin/magento setup:static-content:deploy`
+- Clean the cache by running `php bin/magento cache:clean`
+- Setup Partner Program ID via Magento Admin panel
+- You are ready to go
 
 ### Module settings in Magento Admin
 
 ![Partner Ads Magento module settings in Magento backend](https://intelligo.link/jc/2022-02-3MQ23gE6kI.png)
 
--   Login to Magento admin   
--   Go to Stores - Configuration - Partner Ads Setup  
--   Enter Partner Program ID   
--   Change settings according to your stores setup
+- Login to Magento admin
+- Go to Stores - Configuration - Partner Ads Setup
+- Enter Partner Program ID
+- Change settings according to your stores setup
 
 ### Multi store setup
 
-The Partner Ads module supports multi store setup per Magento’s normal requirements, therefore you are able to set individual Partner Ads program on a per store view basis via Magento admin.  
-  
+The Partner Ads module supports multi store setup per Magento’s normal requirements, therefore you are able to set individual Partner Ads program on a per store view basis via Magento admin.
 
 ### Debugging
+
 Starting from version 2.1.0, we have made significant enhancements to the debug log functionality. To effectively debug Partner Ads tracking and communication with Partner Ads servers, please follow these steps:
 
--   Login to Magento admin   
--   Go to Stores - Configuration - Partner Ads Setup  
--   Adjust the `Mode` setting to `Debug`
--   Perform an order placement using valid `PACID` and `PARTNERID` values
--   Monitor the request and response log in the `var/log/Partnerads.log` file
+- Login to Magento admin
+- Go to Stores - Configuration - Partner Ads Setup
+- Adjust the `Mode` setting to `Debug`
+- Perform an order placement using valid `PACID` and `PARTNERID` values
+- Monitor the request and response log in the `var/log/Partnerads.log` file
 
-Please remember to set `Mode` back to `Production` afterwards. 
+Please remember to set `Mode` back to `Production` afterwards.
 
 ## Support & Bugs
 
